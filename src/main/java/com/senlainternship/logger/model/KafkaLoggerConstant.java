8dddc7df96190444e341aa62ff9cc72a3812f430
Lesha Korobko <karabonbl4@gmail.com>
2024-03-09 11:25:28 +0300

    fixed constant class

--- src/main/java/com/senlainternship/logger/model/KafkaLoggerConstant.java
+++ src/main/java/com/senlainternship/logger/model/KafkaLoggerConstant.java
@@ -1,14 +1,17 @@
 package com.senlainternship.logger.model;
 
+import lombok.experimental.UtilityClass;
+
 import java.time.format.DateTimeFormatter;
 
-public interface KafkaLoggerConstant {
+@UtilityClass
+public class KafkaLoggerConstant {
 
-    DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("dd-MM-yyyy hh:mm:ss");
+    public static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("dd-MM-yyyy hh:mm:ss");
 
-    String ARGS = "ARGS";
+    public static final String ARGS = "ARGS";
 
-    String RETURNS = "RETURNS";
+    public static final String RETURNS = "RETURNS";
 
-    String WARNING = "WARNING";
+    public static final String WARNING = "WARNING";
 }
